99da9a2312fa75fbc2cf783bf3c3763ddce510c7
AnshhSingh <anshsingh25bd@gmail.com>
2023-04-18 19:53:44 +0530

    bug fixes

--- arch.sh
+++ arch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Main script to launch child scripts for other tasks
 ./scripts/startup.sh|&tee startup.log
-./scripts/archnstall.sh|&tee archinstall.log
-./scripts/postinstall.sh|&tee postinstall.log
+./scripts/archinstall.sh|&tee archinstall.log
 echo "done!!"
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,7 +5,7 @@ pacmansetup(){
     pacman-key --init
     pacman-key --populate archlinux
     pacman -S --noconfirm archlinux-keyring
-    pacman -S --needed base-devel git
+    pacman -S --noconfirm --needed base-devel git
     sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
     pacman -S --noconfirm --needed reflector rsync grub
     cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
