08941bababc94bf2027bf0c2e8106488159460ad
AnshhSingh <anshsingh25bd@gmail.com>
2023-04-18 19:32:34 +0530

    implimented use of archinstall

--- arch.sh
+++ arch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Main script to launch child scripts for other tasks
 ./scripts/startup.sh|&tee startup.log
-./scripts/preinstall.sh|&tee preinstall.log
+./scripts/archnstall.sh|&tee archinstall.log
+./scripts/postinstall.sh|&tee postinstall.log
 echo "done!!"
---
+++ scripts/archinstall.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+aurhelp(){
+    git clone https://aur.archlinux.org/yay.git
+    makepkg -si --noconfirm
+}
+aurhelp
+archinstall
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Essential Services
+-------------------------------------------------------------------------
+"
+systemctl enable cups.service
+echo "  Cups enabled"
+ntpd -qg
+systemctl enable ntpd.service
+echo "  NTP enabled"
+systemctl disable dhcpcd.service
+echo "  DHCP disabled"
+systemctl stop dhcpcd.service
+echo "  DHCP stopped"
+systemctl enable NetworkManager.service
+echo "  NetworkManager enabled"
+systemctl enable bluetooth
+echo "  Bluetooth enabled"
+systemctl enable avahi-daemon.service
+echo "  Avahi enabled"
