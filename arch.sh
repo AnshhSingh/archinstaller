1b4f00cb504af57db19537fbaf16f5ba6d2dfb95
Ansh <ANSHSINGH25BD@GMAIL.COM>
2023-04-18 21:43:21 +0530

    Bug fixes

--- arch.sh
+++ arch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Main script to launch child scripts for other tasks
+chmod +x scripts/startup.sh
+chmod +x scripts/archinstall.sh
 ./scripts/startup.sh|&tee startup.log
 ./scripts/archinstall.sh|&tee archinstall.log
 echo "done!!"
